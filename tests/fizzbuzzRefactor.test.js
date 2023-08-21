9eb6cd3dc0051cf1b2bfb025a44c3cc63db68906
Paul Fletes <paul.fletes@gmail.com>
2023-08-21 18:34:50 -0500

    add customFB test coverage

--- tests/fizzbuzzRefactor.test.js
+++ tests/fizzbuzzRefactor.test.js
@@ -30,3 +30,39 @@ test("#fizzBuzz object displays correct buzz count", () => {
 test("#fizzBuzz object displays correct fizzBuzz count", () => {
   expect(fizzbuzz.fizzBuzz(30).fizzBuzz).toBe(2);
 })
+// describe('#customFizzBuzz function', () => {
+//   test('generates foo and bar counts correctly', () => {
+//     const result = customFizzBuzz(20, 3, 'foo', 5, 'bar');
+//     expect(result.count).toBe(20);
+//     expect(result.foo).toBe(5);
+//     expect(result.bar).toBe(3);
+//     expect(result.foobar).toBe(1);
+//     expect(result['']).toBe(11);
+//   });
+
+//   test('handles custom multiples and strings', () => {
+//     const result = customFizzBuzz(15, 2, 'even', 7, 'odd');
+//     expect(result.count).toBe(15);
+//     expect(result.even).toBe(7);
+//     expect(result.odd).toBe(3);
+//     expect(result.evenodd).toBe(1);
+//     expect(result['']).toBe(4);
+//   });
+
+//   test('handles different count values', () => {
+//     const result = customFizzBuzz(5, 3, 'fizz', 5, 'buzz');
+//     expect(result.count).toBe(5);
+//     expect(result.fizz).toBe(1);
+//     expect(result.buzz).toBe(1);
+//     expect(result.fizzbuzz).toBe(0);
+//     expect(result['']).toBe(3);
+//   });
+
+//   test('handles multipleA being equal to multipleB', () => {
+//     const result = customFizzBuzz(10, 2, 'even', 2, 'twice');
+//     expect(result.count).toBe(10);
+//     expect(result.even).toBe(5);
+//     expect(result.twice).toBe(5);
+//     expect(result['']).toBe(0);
+//   });
+// });
